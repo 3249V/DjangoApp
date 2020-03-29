7f19810efefdf4faa6262c91464cc88b9b109413
Connor <awsmspwn@gmail.com>
2020-03-29 11:07:32 -0700

    "stuff fixed 2.0"

--- DjangoLearning/settings.py
+++ DjangoLearning/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from typing import Optional, Any
 
@@ -155,4 +155,5 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+django_heroku.settings(locals())
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
