1c9a533e4b3e9eef9261922d219be8ec11260988
3249V <55768669+3249V@users.noreply.github.com>
2021-01-29 09:48:22 -0800

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    media = models.FileField(upload_to='mememedia/')
 
     def __str__(self):
         return self.title
