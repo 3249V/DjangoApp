b867b29b667ec01e61b5ab4b32abfd7c22cfba01
3249V <55768669+3249V@users.noreply.github.com>
2021-01-29 10:18:53 -0800

    media? (#7)
    * "stuff fixed 4.0"
    
    * "stuff fixed 4.0"
    
    * Images?
    
    * Media?
    
    * WORK PLEASE
    
    * Migrations for media
    
    * aaaaaaa work
    
    Co-authored-by: Connor <awsmspwn@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+
+    media = models.FileField(upload_to='mememedia/')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     media = models.FileField(upload_to='mememedia/',default="")
