9643e21491cdb6c2b7a7d6eec6ef34f402df9493
3249V <55768669+3249V@users.noreply.github.com>
2021-01-29 09:17:50 -0800

    Update settings.py

--- DjangoLearning/settings.py
+++ DjangoLearning/settings.py
@@ -147,7 +147,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 print(os.environ.get('AWS_ACCESS_KEY_ID'))
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 print(os.environ.get('AWS_SECRET_ACCESS_KEY'))
-AWS_STORAGE_BUCKET_NAME = 'djangowebapp'
+AWS_STORAGE_BUCKET_NAME = 'mememakerceo'
 print(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
 AWS_S3_REGION_NAME = 'us-west-2'
 
