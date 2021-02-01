e975eb5a766d3f56a769ddd8de5cb714839bf62c
Connor <awsmspwn@gmail.com>
2021-02-01 09:05:22 -0800

    Timezones

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from blog.models import Post
+from django.core.exceptions import ValidationError
+
+class TestForm(forms.ModelForm):
+    def clean_file(self, form):
+        megabyte_limit = 5.0
+        media = self.cleaned_data.get('media', False)
+        if media._size > megabyte_limit * 1024 * 1024:
+            raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
+    class Meta:
+        model = Post
--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-
 class Post(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField(max_length=100, blank=True)
-
+    form_class = TestForm
     media = models.FileField(upload_to='mememedia/')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,3 +17,5 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
